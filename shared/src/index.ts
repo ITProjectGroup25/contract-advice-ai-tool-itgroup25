f7596957c1f79d4bb771d2505557efec55389dcb
API Key Tester <yinghuaf@student.unimelb.edu.au>
2025-10-01 01:56:21 +1000

    fix: export all required types from @shared module for CI/CD

--- shared/src/index.ts
+++ shared/src/index.ts
@@ -1,2 +1,8 @@
+// Export form types
 export * from "./types/forms";
+
+// Export dynamic form types
+export * from "./types/dynamic-form";
+
+// Export default data
 export * from "./data/defaultQuestions";
